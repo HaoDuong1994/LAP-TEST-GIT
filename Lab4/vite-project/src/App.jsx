b6d28862632f28eb5658605681829730f21924b3
HaoDuong1994 <hao.dv5370@gmail.com>
2023-07-27 17:34:43 +0700

    done change

--- Lab4/vite-project/src/App.jsx
+++ Lab4/vite-project/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [heroCurrent, setHeroValue] = useState('https://i.pinimg.com/1200x/5b/f4/a9/5bf4a989ac96489a138b7f2dc223ebd0.jpg');
+  const [heroCurrent, setHeroValue] = useState('https://cdn.vox-cdn.com/thumbor/SbX1VbxJhxijxD1tzRTJ8uq38P4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19101461/spider_man_far_from_home_peter_parker_1562394390.jpg');
   let heroList = [
     "https://i.pinimg.com/1200x/5b/f4/a9/5bf4a989ac96489a138b7f2dc223ebd0.jpg",
     "https://cdn.vox-cdn.com/thumbor/SbX1VbxJhxijxD1tzRTJ8uq38P4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19101461/spider_man_far_from_home_peter_parker_1562394390.jpg",
@@ -20,16 +20,30 @@ function App() {
     setHeroValue(randomSuperHero)
   }
 
+  function handleSpidey(){
+    setHeroValue('https://cdn.vox-cdn.com/thumbor/SbX1VbxJhxijxD1tzRTJ8uq38P4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19101461/spider_man_far_from_home_peter_parker_1562394390.jpg')
+  }
+  function handleCaptainMarvel(){
+    setHeroValue('https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_.jpg')
+  }
+
+  function handleIronman (){
+    setHeroValue('https://i.pinimg.com/550x/5d/d2/c8/5dd2c8f82ef46e0db3fe07a6e3285c2f.jpg')
+  }
+
+  function handleWanda (){
+    setHeroValue('https://i.bbcosplay.com/14934/scarlet-whitch.jpg')
+  }
   return (
     <>
       <div>
         <img className="main-image" src={heroCurrent}></img>
       </div>
       <div>
-        <img className="item-image"src="https://cdn.vox-cdn.com/thumbor/SbX1VbxJhxijxD1tzRTJ8uq38P4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19101461/spider_man_far_from_home_peter_parker_1562394390.jpg"></img>
-        <img className="item-image"src="https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_.jpg"></img>
-        <img className="item-image"src="https://i.pinimg.com/550x/5d/d2/c8/5dd2c8f82ef46e0db3fe07a6e3285c2f.jpg"></img>
-        <img className="item-image"src="https://i.bbcosplay.com/14934/scarlet-whitch.jpg"></img>
+        <img onClick ={handleSpidey} className="item-image"src="https://cdn.vox-cdn.com/thumbor/SbX1VbxJhxijxD1tzRTJ8uq38P4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19101461/spider_man_far_from_home_peter_parker_1562394390.jpg"></img>
+        <img onClick ={handleCaptainMarvel}className="item-image"src="https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_.jpg"></img>
+        <img onClick={handleIronman}className="item-image"src="https://i.pinimg.com/550x/5d/d2/c8/5dd2c8f82ef46e0db3fe07a6e3285c2f.jpg"></img>
+        <img onClick={handleWanda}className="item-image"src="https://i.bbcosplay.com/14934/scarlet-whitch.jpg"></img>
       </div>
       <br></br>
       <button className="btn"onClick={handleSuperHero} >Change Super Hero</button>
