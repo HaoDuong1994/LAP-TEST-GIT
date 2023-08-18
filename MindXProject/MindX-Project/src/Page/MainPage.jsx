959c44e5401f1c5d95a1c85f59f0d7687752be31
HaoDuong1994 <hao.dv5370@gmail.com>
2023-08-18 18:37:29 +0700

    update mainpage

--- MindXProject/MindX-Project/src/Page/MainPage.jsx
+++ MindXProject/MindX-Project/src/Page/MainPage.jsx
@@ -1,8 +1,30 @@
+import CoverImg2 from "../assets/img/coverPhoto.jpg";
 import CoverImg from "../assets/img/fashion-sale-social-media-cover-post-banner-template_636037-97.jpg";
 function MainPage() {
   return (
-    <div>
-      <img src={CoverImg} />
+    <div className="main-page">
+      <img className="cover-img" src={CoverImg2} />
+      <div className="main-context">
+        <h2>Giới Thiêu</h2>
+        <h4>I. Lịch sử hình thành</h4>
+        <p>
+          Adam Store ra đời từ 2012, khởi đầu ở một cửa hàng nhỏ trên phố Lò
+          Đúc. Adam Store những ngày đầu được gây dựng bởi 3 chàng trai trẻ
+          tuổi, ôm trong mình hoài bão thay đổi xu hướng thời trang Việt Nam.
+          Bằng đam mê với vest và bằng những cố gắng bền bỉ trong từng giây
+          phút, Adam Store đã tiên phong trong ngành thời trang, thay đổi tư duy
+          khách hàng, trở thành thương hiệu Việt Nam lớn nhất trong thị phần
+          vest may sẵn, là thương hiệu uy tín về đồ Âu cho nam giới.
+        </p>
+        <h4>II. 5 lý do khách hàng nên mua sản phẩm của Adam Store</h4>
+        <h4>1. Nhiều size nhất, sát người dùng nhất</h4>
+        <p>
+          Nếu các thương hiệu khác chỉ có 3 sizes cho khách hàng thì ở Adam
+          Store có đến 5 sizes phù hợp cho người từ 55-85kg. Chúng tôi muốn đảm
+          bảo bạn luôn có thể chọn được một chiếc áo mặc vừa với người.
+        </p>
+        <img />
+      </div>
     </div>
   );
 }
