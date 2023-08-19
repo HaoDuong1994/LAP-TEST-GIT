a17c102c9a094ad47d7f11d8eda23326f51865bb
HaoDuong1994 <hao.dv5370@gmail.com>
2023-08-19 16:57:19 +0700

    update

--- MindXProject/MindX-Project/src/Component/Product/ProductList.jsx
+++ MindXProject/MindX-Project/src/Component/Product/ProductList.jsx
@@ -1,8 +1,34 @@
 import ProductItem from "./ProductItem";
 import { productList } from "./productsList";
+import "./product.css";
 
 function ProductList() {
   console.log(productList);
-  return <div>Hello from Product List</div>;
+  return (
+    <div className="product-list-wrapper">
+      <img
+        className="product-img-cover"
+        src="https://as1.ftcdn.net/v2/jpg/05/29/33/80/1000_F_529338076_XnKogxVjg4jnOxzY3WB5LANCcyYcucbb.jpg"
+      />
+      <h2>Tất cả sản phẩm</h2>
+      <div className="list-item-product">
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </div>
+  );
 }
 export default ProductList;
